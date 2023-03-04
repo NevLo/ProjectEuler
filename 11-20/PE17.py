5124da913d2dbdaac1b50a182882a3a987b315df
NevLo <pilleychristian@gmail.com>
2023-03-04 05:24:07 -0800

    Update PE17.py
    small changes, not finished

--- 11-20/PE17.py
+++ 11-20/PE17.py
@@ -6,11 +6,11 @@
 # NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters 
 # and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 
-numDict = {1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine",
-             11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen", 15: "Fifteen", 16: "Sixteen", 17: "Seventeen",
+numDict =    {1: "One",       2: "Two",        3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine",
+             11: "Eleven",   12: "Twelve",    13: "Thirteen", 14: "Fourteen", 15: "Fifteen", 16: "Sixteen", 17: "Seventeen",
              18: "Eighteen", 19: "Nineteen",
-             10: "Ten", 20: "Twenty", 30: "Thirty", 40: "Forty", 50: "Fifty", 60: "Sixty", 70: "Seventy", 80: "Eighty",
-             90: "Ninety", 100: "Hundred", 1000: "Thousand"}
+             10: "Ten",      20: "Twenty",    30: "Thirty", 40: "Forty", 50: "Fifty", 60: "Sixty", 70: "Seventy", 80: "Eighty",
+             90: "Ninety",  100: "Hundred", 1000: "Thousand"}
 
 
 
@@ -18,4 +18,15 @@ if __name__ == "__main__":
     sum = 0
 
     for i in range(1001):
-        ...
+        if(i < 101):
+            if(i not in numDict):
+                ...
+                
+        elif(i<1001):
+            ...
+
+    for i in range(len(numDict)):
+        if(i in numDict):
+            sum += len(numDict.get(i))
+
+    print(sum)
